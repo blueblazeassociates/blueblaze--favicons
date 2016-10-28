b672b363dadc2026f0e3f3ca69b2491884ffaf67
Ed Gifford <egifford@blueblazeassociates.com>
2016-10-28 14:07:36 -0400

    Updating blueblaze__favicons() with new plugin files.

--- blueblaze--favicons.php
+++ blueblaze--favicons.php
@@ -31,17 +31,13 @@ function blueblaze__favicons() {
   $base_uri = plugins_url( '', __FILE__ );
 
   $output = <<<HTML
-<link rel="apple-touch-icon" sizes="180x180" href="$base_uri/assets/favicons/apple-touch-icon.png">
-<link rel="icon" type="image/png" href="$base_uri/assets/favicons/favicon-32x32.png" sizes="32x32">
-<link rel="icon" type="image/png" href="$base_uri/assets/favicons/favicon-194x194.png" sizes="194x194">
-<link rel="icon" type="image/png" href="$base_uri/assets/favicons/android-chrome-192x192.png" sizes="192x192">
-<link rel="icon" type="image/png" href="$base_uri/assets/favicons/favicon-16x16.png" sizes="16x16">
-<link rel="manifest" href="$base_uri/assets/favicons/manifest.json">
-<link rel="mask-icon" href="$base_uri/assets/favicons/safari-pinned-tab.svg" color="#2f69b3">
-<link rel="shortcut icon" href="$base_uri/assets/favicons/favicon.ico">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="$base_uri/assets/favicons/mstile-144x144.png">
-<meta name="msapplication-config" content="$base_uri/assets/favicons/browserconfig.xml">
+<link rel="apple-touch-icon" sizes="180x180" href="$base_uri/assets/favicons/apple-touch-icon.png?v=wAvrq8rOyQ">
+<link rel="icon" type="image/png" href="$base_uri/assets/favicons/favicon-32x32.png?v=wAvrq8rOyQ" sizes="32x32">
+<link rel="icon" type="image/png" href="$base_uri/assets/favicons/favicon-16x16.png?v=wAvrq8rOyQ" sizes="16x16">
+<link rel="manifest" href="$base_uri/assets/favicons/manifest.json?v=wAvrq8rOyQ">
+<link rel="mask-icon" href="$base_uri/assets/favicons/safari-pinned-tab.svg?v=wAvrq8rOyQ" color="#2e67b2">
+<link rel="shortcut icon" href="$base_uri/assets/favicons/favicon.ico?v=wAvrq8rOyQ">
+<meta name="msapplication-config" content="$base_uri/assets/favicons/browserconfig.xml?v=wAvrq8rOyQ">
 <meta name="theme-color" content="#ffffff">
 HTML;
 
